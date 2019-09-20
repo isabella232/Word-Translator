0c0b3619d9080a26d9b89c7de4bdc892aee68384
jhvoong <jhvoong@ucdavis.edu>
2019-09-19 21:33:25 -0700

    Add files via upload

---
+++ student_starter/word_translator_ss.py
@@ -0,0 +1,40 @@
+def createAlphabet(charEncoding):
+    ''' Turns a 26 character string called charEncoding into a dictionary,
+        pairing each English letter to each character
+
+        Input: "abcdefghijklmnopqrstuvwxyz"
+        Output: {0: 'a', 1: 'b', 2: 'c', ... , 24: 'y', 25: 'z'}
+    '''
+
+    # declare necessary variables
+
+    # create the new alphabet
+
+    # TODO: some dummy data, delete this line when finished
+    return {'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D', 'e': 'E', 'f': 'F',
+            'g': 'G', 'h': 'H', 'i': 'I', 'j': 'J', 'k': 'K', 'l': 'L',
+            'm': 'M', 'n': 'N', 'o': 'O', 'p': 'P', 'q': 'Q', 'r': 'R',
+            's': 'S', 't': 'T', 'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X',
+            'y': 'Y', 'z': 'Z'}
+
+
+def translate(alphabet, word):
+    ''' Uses a dictionary called alphabet to translate word
+        Does not modify original word
+        
+        Inputs: Dictionary, string
+        Output: string
+    '''
+
+    # TODO: some dummy data, delete this line when finished
+    return "TODO"
+
+
+def main():
+    ''' Translates a single word into another word using a new alphabet '''
+
+    # obtain user input
+        
+    # generate pseudo-alphabet and store into dictionary
+
+    # translate words until user ends program
---
+++ word_translator.py
@@ -0,0 +1,70 @@
+def createAlphabet(charEncoding):
+    ''' Turns a 26 character string called charEncoding into a dictionary,
+        pairing each English letter to each character
+
+        Input: "abcdefghijklmnopqrstuvwxyz"
+        Output: {0: 'a', 1: 'b', 2: 'c', ... , 24: 'y', 25: 'z'}
+    '''
+
+    # declare necessary variables
+    newAlphabet = {}
+    engAlphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    # create the new alphabet
+    i = 0
+    for newChar in charEncoding:
+        engChar = engAlphabet[i]
+        newAlphabet[engChar] = newChar
+        i += 1
+
+    return newAlphabet
+
+def translate(alphabet, word):
+    ''' Uses a dictionary called alphabet to translate word
+        Does not modify original word
+
+        alphabet has elements (englishChar : translatedChar)
+        word is a string
+        
+        Inputs: Dictionary, string
+        Output: string
+    '''
+
+    newWord = ""
+    for char in word:
+        newWord += alphabet[char]
+
+    return newWord
+
+def main():
+    ''' Translates a single word into another word using a new alphabet
+        First, creates dictionary of new alphabet, then continuously asks
+        user for words to translate and prints out the translation until they
+        type Enter
+    '''
+
+    # obtain user input
+    print("Please enter 26 characters for your new alphabet")
+    print("The first character replaces 'a', and the last character replaces 'z'")
+    encoding = input()
+
+    if (len(encoding) != 26):
+        print("Not 26 characters")
+        return
+        
+    # generate pseudo-alphabet and store into dictionary
+    newAlphabet = createAlphabet(encoding)
+
+    # translate words until user ends program
+    while (True):
+        print("Enter a word to translate using the new encoding (Enter to exit)")
+
+        engWord = input("Old Word: ")
+
+        if (engWord == ""):
+            break
+        
+        # generate word using newly defined alphabet
+        word = translate(newAlphabet, engWord)
+
+        print("New Word: " + word + "\n")
